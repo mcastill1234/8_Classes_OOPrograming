d7a985f5c4f4f8bc9aa2237678ad5cded257e74f
mcastill1234 <30333120+mcastill1234@users.noreply.github.com>
2017-08-07 21:18:12 -0400

    Found error in TwoRate, corrected Mortgages spelling

--- Mortage.py
+++ Mortage.py
@@ -1,20 +1,20 @@
 def findPayment(loan, r, m):
-    """Assumes: loan and r are floats, m an int, returns the monthly payment for a mortage of
+    """Assumes: loan and r are floats, m an int, returns the monthly payment for a mortgage of
     size loan at a monthly rate r for m months."""
     return loan*((r*(1 + r)**m)/((1 + r)**m - 1))
 
 
-class Mortage(object):
-    """Abstract class for building different kinds of mortages"""
+class Mortgage(object):
+    """Abstract class for building different kinds of mortgges"""
     def __init__(self, loan, annRate, months):
-        """Create a new mortage"""
+        """Create a new mortgage"""
         self.loan = loan
         self.rate = annRate/12.0
         self.months = months
         self.paid = [0.0]
         self.owed = [loan]
         self.payment = findPayment(loan, self.rate, months)
-        self.legend = None  #description of mortage
+        self.legend = None  # description of mortgage
 
     def makePayment(self):
         """Make a payment"""
@@ -23,54 +23,56 @@ class Mortage(object):
         self.owed.append(self.owed[-1] - reduction)
 
     def getTotalPaid(self):
-        """Returm the total amount of paid so far"""
+        """Return the total amount of paid so far"""
         return sum(self.paid)
 
     def __str__(self):
         return self.legend
 
 
-class Fixed(Mortage):
+class Fixed(Mortgage):
     def __init__(self, loan, r, months):
-        Mortage.__init__(self, loan, r, months)
+        Mortgage.__init__(self, loan, r, months)
         self.legend = 'Fixed, ' + str(r*100) + '%'
 
 
-class FixedWithPts(Mortage):
+class FixedWithPts(Mortgage):
     def __init__(self, loan, r, months, pts):
-        Mortage.__init__(self, loan, r, months)
+        Mortgage.__init__(self, loan, r, months)
         self.pts = pts
         self.paid = [loan*pts/100.0]
         self.legend = 'Fixed, ' + str(r*100) + '%, ' + str(pts) + ' points'
 
 
-class TwoRate(Mortage):
-    def __int__(self, loan, r, months, teaserRate, teaserMonths):
-        Mortage.__init__(self, loan, teaserRate, months)
+class TwoRate(Mortgage):
+    def __init__(self, loan, r, months, teaserRate, teaserMonths):
+        Mortgage.__init__(self, loan, teaserRate, months)
         self.teaserMonths = teaserMonths
         self.teaserRate = teaserRate
-        self.nextRate = r/12.0
-        self.legend = str(teaserRate*100) + '% for ' + str(self.teaserMonths)\
-                      + 'months, then ' + str(r*100) + '%'
+        self.nextRate = r / 12.0
+        self.legend = str(teaserRate * 100) \
+                  + '% for ' + str(self.teaserMonths) \
+                  + ' months, then ' + str(r * 100) + '%'
 
     def makePayment(self):
         if len(self.paid) == self.teaserMonths + 1:
             self.rate = self.nextRate
-            self.payment = findPayment(self.owed[ -1], self.rate, self.months - self.teaserMonths)
-        Mortage.makePayment(self)
+            self.payment = findPayment(self.owed[-1], self.rate, self.months - self.teaserMonths)
+        Mortgage.makePayment(self)
 
 
-def compareMortages(amt, years, fixedRate, pts, ptsRate, varRate1, varRate2, varMonths):
+def compareMortgages(amt, years, fixedRate, pts, ptsRate, varRate1, varRate2, varMonths):
     totMonths = years*12
     fixed1 = Fixed(amt, fixedRate, totMonths)
     fixed2 = FixedWithPts(amt, ptsRate, totMonths, pts)
     twoRate = TwoRate(amt, varRate2, totMonths, varRate1, varMonths)
     morts = [fixed1, fixed2, twoRate]
     for m in range(totMonths):
-        mort.makePayment()
+        for mort in morts:
+            mort.makePayment()
     for m in morts:
         print(m)
         print(' Total payments = $' + str(int(m.getTotalPaid())))
 
-compareMortages(amt=200000, years=30, fixedRate=0.07, pts=3.25, ptsRate=0.05,
+compareMortgages(amt=200000, years=30, fixedRate=0.07, pts=3.25, ptsRate=0.05,
                 varRate1=0.045, varRate2=0.095, varMonths=48)
